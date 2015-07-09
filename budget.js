dd24d8c97cc38c7e2996624d9c78481edfae69db
Ominix <ryan.buck1989@gmail.com>
2015-07-08 23:29:29 -0400

    I have just put my whole project on GitHub

---
+++ budget.js
@@ -0,0 +1,12 @@
+//Accepts the amount of one bill | will be cleared after every entry
+var bill = 0;
+
+//The name of the bill 
+var billName = "";
+
+//grand total of all monthly bills
+var totalMonthlyBills = 0; 
+
+// array holding the name and amount of the bill 
+var billList = [[""],[0]]; 
+
